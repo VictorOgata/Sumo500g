bf44605c81f443d1e0e7479c9688094676bf185e
Unknown <victoroliveiraogata@hotmail.com>
2020-03-04 23:47:58 -0300

    teste

--- Desktop/sumo500g/Codigosumo500g/CodigoSumoFinal/CodigoSumoFinal.ino
+++ Desktop/sumo500g/Codigosumo500g/CodigoSumoFinal/CodigoSumoFinal.ino
@@ -40,44 +40,50 @@ void Stop()
   }
 void motorF()
 {
-  digitalWrite(PB9, LOW);
- digitalWrite(PB8,HIGH );
+  digitalWrite(PB9,LOW);
+ digitalWrite(PB8,LOW );
  digitalWrite(PB7,HIGH);
- digitalWrite(PB6 ,LOW);
+ digitalWrite(PB6 ,HIGH);
 }
 void motorB()
 {
- digitalWrite(PB9, HIGH);
- digitalWrite(PB8,LOW );
- digitalWrite(PB7, LOW);
- digitalWrite(PB6 ,HIGH);
+  digitalWrite(PB9,HIGH);
+ digitalWrite(PB8,HIGH );
+ digitalWrite(PB7,LOW);
+ digitalWrite(PB6 ,LOW);
 }
 void motorL()
 {
- digitalWrite(PB9,HIGH);
- digitalWrite(PB8,LOW );
- digitalWrite(PB7,HIGH);
- digitalWrite(PB6 ,LOW);
-}
-void motorR()
-{
- digitalWrite(PB9, LOW);
+ digitalWrite(PB9,LOW);
  digitalWrite(PB8,HIGH );
  digitalWrite(PB7,LOW);
  digitalWrite(PB6 ,HIGH);
+}
+void motorR()
+{
+ digitalWrite(PB9, HIGH);
+ digitalWrite(PB8,LOW );
+ digitalWrite(PB7,HIGH);
+ digitalWrite(PB6 ,LOW);
 
 }
 
 void loop(){
-  L = digitalRead(PB1);
-  R = digitalRead(PB0);
-if(L==0 && R==0)  
+  L = digitalRead(PA7);
+  R = digitalRead(PA6);
+//  Serial.println(L);
+//   Serial.println(R);
+ //   digitalWrite(PB9,LOW);
+// digitalWrite(PB8,HIGH );
+// digitalWrite(PB7,HIGH);
+// digitalWrite(PB6 ,LOW);
+if(L==1 && R==1)  
 motorF();
-else if(L==1 && R==0)
-motorL(); 
 else if(L==0 && R==1)
+motorL(); 
+else if(L==1 && R==0)
 motorR();
-else if(L==1 && R==1)
+else if(L==0 && R==0)
 Stop();
 
 
